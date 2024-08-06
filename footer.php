c443834472eba2a4d4435833a8486418a4e4faaf
MughA1l <rohailm288@gmail.com>
2024-08-06 17:15:39 +0500

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,38 @@
+<div class="container-fluid bg-white mt-5">
+  <div class="row">
+   <div class="col-lg-4 p-4">
+    <h3 class="h-font fw-bold fs-3 mb-2">MUGHAL HOTEL</h3>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+       Officiis doloremque corporis, illum tenetur aut quam animi 
+       beatae ipsa error saepe odit, tempora commodi cumque 
+       pariatur libero? Neque dolor magni voluptatibus?
+
+    </p>
+   </div>
+   <div class="col-lg-4 p-4">
+     <h3 class="mb-3 text-dark">Links</h3>
+     <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Home</a><br>
+     <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Rooms</a><br>
+     <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Facilities</a><br>
+     <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Contect us</a><br>
+     <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">About</a>
+   </div>
+   <div class="col-lg-4 p-4">
+    <h3 class="mb-3">Follow us</h3>
+    <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">
+       <i class="bi bi-twitter me-1"></i> Twitter
+      </a><br>
+      <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">
+        <i class="bi bi-facebook me-1"></i> Facebook
+       </a><br>
+       <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">
+        <i class="bi bi-instagram me-1"></i> Instagram
+       </a>
+   </div>
+  </div>
+</div>
+
+<h6 class="text-center bg-dark text-white p-3 m-0">Designed and Developed by MUGHAL WEBDEV</h6>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"  crossorigin="anonymous"></script>
+
